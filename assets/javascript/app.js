00530241b44569529dfb31d5bd31107362eb447a
Madeleine Prak <madeleineprak@gmail.com>
2019-08-27 00:54:41 -0700

    set up some files and more format

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+// $('a[href*="#"]').on('click', function(e) {
+//   e.preventDefault()
+
+//   $('html, body').animate(
+//     {
+//       scrollTop: $($(this).attr('href')).offset().top,
+//     },
+//     500,
+//     'linear'
+//   )
+// })
+
+//OR THIS W3SCHOOLS SOLUTION
+$(document).ready(function () {
+    $(".nav-item a").on("click", function (event) {
+        event.preventDefault();
+        $(".navbar-nav li").removeClass("active");
+        $(this).parent().addClass("active");
+    })
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
