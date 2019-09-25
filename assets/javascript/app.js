58cad0fba6cf1b88919dec97ab830b6b403eeb7b
Madeleine Prak <madeleineprak@gmail.com>
2019-09-25 00:38:01 -0700

    Change layout, add color scheme, add updated brand statement, add project screenshots and description, add social and project links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,43 @@
 // Credit to w3schools
 $(document).ready(function () {
+    var firebaseConfig = {
+        apiKey: "AIzaSyCa4oCiOi8ZhtUe2ihhXjDvdszdomgVWe0",
+        authDomain: "web-portfolio-b48e7.firebaseapp.com",
+        databaseURL: "https://web-portfolio-b48e7.firebaseio.com",
+        projectId: "web-portfolio-b48e7",
+        storageBucket: "",
+        messagingSenderId: "1021843626708",
+        appId: "1:1021843626708:web:4e5ad6a53dbd346f7d3941"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
+
+    var userMessage = {
+        name: "",
+        email: "",
+        message: ""
+    }
+
+    $("#submit-button").on("click", function(event) {
+        event.preventDefault();
+        console.log("Message sent");
+
+        // Need to add form validation -- parsley.js
+        userMessage.name = $("#name-input").val().trim();
+        userMessage.email = $("#email-input").val().trim();
+        userMessage.message = $("#message-input").val().trim();
+
+        database.ref().push({
+            userMessage: userMessage,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        $("#name-input").val("");
+        $("#email-input").val("");
+        $("#message-input").val("");
+    })
+
     $(".nav-item a").on("click", function (event) {
         event.preventDefault();
         $(".navbar-nav li").removeClass("active");
