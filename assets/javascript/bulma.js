127ae8b36a6fe5b37a46d21a434968ec87bd4d88
Madeleine Prak <madeleineprak@gmail.com>
2019-12-14 12:59:17 -0800

    Add memory game project

--- assets/javascript/bulma.js
+++ assets/javascript/bulma.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 
     var projects = [
         {
+            name: "Memory Game",
+            photo: "./assets/images/MemoryGame.PNG",
+            gif: "./assets/images/MemoryGame.gif",
+            description: "A game where the user's goal is to click a unique image each time to get the most points.",
+            ideation: "",
+            tools: ["React", "Unsplash API", "Bulma", "bulma-toast"],
+            github: "https://github.com/madeleineprak/Memory-Game",
+            website: "https://madeleineprak.github.io/Memory-Game/"
+        },
+        {
             name: "Get The Scoop",
             photo: "./assets/images/GetTheScoop.PNG",
             gif: "./assets/images/GetTheScoop.gif",
